d5dced492b0760d6435570080120c30da04d6653
elissa_liu <524948583@qq.com>
2022-07-15 17:27:51 +0800

    init: 初始配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,20 +7,23 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    redirect: '/gis2d',
+    meta: {
+      headerDisplay: true
+    },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/gis2d',
+    name: 'gis2d',
+    meta: {
+      headerDisplay: true
+    },
+    component: () => import('@/views/gis2d.vue')
+  },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
---
+++ src/views/gis2d.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="gis2d">
+    地图
+  </div>
+</template>
+
+<script>
+export default {
+  name: "gis2d",
+  props: {},
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+  watch: {}
+}
+</script>
+<style lang="scss" scoped>
+#gis2d{}
+</style>
