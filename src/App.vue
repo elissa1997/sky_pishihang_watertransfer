b90d4f392e360fde2d0737791d9bc8e76fcabf5f
elissa_liu <524948583@qq.com>
2022-07-22 11:00:59 +0800

    init: 初始化main、App文件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
+    <headerBar v-if="$route.meta.headerDisplay"/>
+    <a-config-provider :locale="locale">
+      <router-view/>
+    </a-config-provider>
   </div>
 </template>
 
+<script>
+import { ConfigProvider } from 'ant-design-vue';
+import zhCN from 'ant-design-vue/es/locale/zh_CN.js';
+import headerBar from "@/views/header.vue";
+
+export default {
+  name: "app",
+  components: {
+    AConfigProvider:ConfigProvider,
+    headerBar
+  },
+  data() {
+    return {
+      locale: zhCN
+    }
+  }
+}
+</script>
 <style lang="scss">
 // body {
 //   margin: 0px !important;
--- src/main.js
+++ src/main.js
@@ -3,10 +3,39 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Dayjs
+import dayjs from "dayjs";
+import dayOfyear from 'dayjs/plugin/dayOfYear';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(dayOfyear)
+dayjs.extend(customParseFormat)
+// echarts
+import * as echarts from 'echarts';
+// 字节跳动IconPark图标库
+import {install} from '@icon-park/vue/es/all';
+// ArcGIS库
+import { initArcGisJs } from "@/util/loadGisModules";
+// Ant Design组件库全局提示
+import { message } from 'ant-design-vue';
+
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const initParamsStart = async () => {
+  try {
+    Vue.prototype.$arcgisModules = await initArcGisJs();
+    Vue.prototype.$message = message;
+    Vue.prototype.$dayjs = dayjs;
+    Vue.prototype.$echarts = echarts;
+  } catch (error) {
+    
+  }
+
+  install(Vue);
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+}
+
+initParamsStart()
